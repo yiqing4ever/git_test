ebfdfd2bdd1b1b743e8e7a4eb390d2b1ebb0f039
yiqing <qing.yi1@pactera.com>
2019-08-15 15:42:31 +0800

    test_demo

---
+++ demo/src/main/java/Hello.java
@@ -0,0 +1,11 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello world！");
+
+        test();
+    }
+
+    public static void test(){
+
+    }
+}

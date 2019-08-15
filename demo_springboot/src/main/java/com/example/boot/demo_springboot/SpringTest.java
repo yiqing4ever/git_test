261a664b0e734d0aca41cb1508b0a3cb39da6654
yiqing <qing.yi1@pactera.com>
2019-08-15 15:29:46 +0800

    test

---
+++ demo_springboot/src/main/java/com/example/boot/demo_springboot/SpringTest.java
@@ -0,0 +1,11 @@
+package com.example.boot.demo_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTest {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTest.class, args);
+    }
+}

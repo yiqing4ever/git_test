4a2411b09b88a2a36d138730e5340137ac18280f
yiqing <qing.yi1@pactera.com>
2019-08-15 15:44:11 +0800

    test_demo

--- demo_springboot/src/main/java/com/example/boot/demo_springboot/SpringTest.java
+++ demo_springboot/src/main/java/com/example/boot/demo_springboot/SpringTest.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringTest {
     public static void main(String[] args) {
+
         SpringApplication.run(SpringTest.class, args);
     }
 }
